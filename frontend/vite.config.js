3c4275d54d2656c8997be4c75e21eadca5274b63
Akanshu06 <akanshu3264@gmail.com>
2025-07-02 16:47:18 +0530

    change url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-	  port: 3000, // Frontend runs on port 3000
-	  proxy: {
-		"/api": {
-		  target: `https://persnal-chat-app.onrender.com` || "http://localhost:5000", // ✅ Change to backend port
-		  changeOrigin: true,
-		  secure: false,
-		},
-		"/socket.io/": {
-		  target:  `https://persnal-chat-app.onrender.com` || "http://localhost:5000" ,
-		  changeOrigin: true,
-		  secure: false,
-		  ws: true,
-		},
-	  },
-	},
-  });
+  plugins: [react()],
+  server: {
+    port: 3000, // Frontend runs on port 3000
+    proxy: {
+      "/api": {
+        target: "https://persnal-chat-app.onrender.com", // ✅ Change to backend port
+        changeOrigin: true,
+        secure: false,
+      },
+      "/socket.io/": {
+        target: "https://persnal-chat-app.onrender.com",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});
