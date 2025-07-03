f1daf23b51f1e9b1177db8948194009e55baf689
Akanshu06 <akanshu3264@gmail.com>
2025-07-03 16:15:54 +0530

    production ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const backendUrl =
-    mode === "production"
-      ? "https://persnal-chat-app.onrender.com"
-      : "http://localhost:5000";
+export default defineConfig(() => {
+  const backendUrl = process.env.VITE_API_URL;
 
   return {
     plugins: [react()],
